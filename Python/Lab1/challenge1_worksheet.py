a44a21f6e57f3bdfad40306eae81fae4d997c35b
Zach Miller <zchatk25966@gmail.com>
2022-04-04 21:02:06 -0700

    Finished challenge1_worksheet.py

--- Python/Lab1/challenge1_worksheet.py
+++ Python/Lab1/challenge1_worksheet.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 ### 0.3 Excercises
+print("0.3 Exercises: ")
 list_1= list(range(1,11))
 print("E1) ", list_1)
 
@@ -34,4 +35,45 @@ def list_shift(base_list, new_data):
 fixed_len_list = [1,2,3,4]
 new_data=[5,6,7]
 print("E6) ", list_shift(fixed_len_list,new_data))
+
+### 1.4 Excercises
+print("1.4 Exercises: ")
+commands = ["STATUS", "ADD", "COMMIT", "PUSH"]
+for x in commands:
+    print(x)
+other_commands=["PUSH FAILED", "BANANAS", "PUSH SUCCESS", "APPLES"]
+text = "SUCCESS"
+print("SUCCESS" in "SUCCESS")  # TRUE
+print("SUCCESS" in "ijoisafjoijiojSUCCESS")  # TRUE
+print("SUCCESS" == "ijoisafjoijiojSUCCESS")  # FALSE
+print("SUCCESS" == text)  # TRUE
+# the == is going character by character and the in is going through the second string and searching for the first
+print("5) the == is going character by character and the in is going through the second string and searching for the first")
+i=0
+while i<len(other_commands):
+    if text in other_commands[i]:
+        print("This worked!")
+        break
+
+    else:
+        print(other_commands[i])
+    i+=1
+
+
+### 2.2 Excercises
+fName="zach"
+byte_name=fName.encode('utf-8')
+byte_name_bad=byte_name+b'\xef'
+
+#4 UnicodeDecodeError: 'utf-8' codec can't decode byte 0xef in position 4: unexpected end of data
+#5
+def decodeMyBytes(byte_to_decode):
+    try:
+        byte_to_decode.decode()
+        return byte_to_decode
+    except UnicodeDecodeError:
+        return ""
+#6
+print(decodeMyBytes(byte_name))
+print(decodeMyBytes(byte_name_bad))
 
