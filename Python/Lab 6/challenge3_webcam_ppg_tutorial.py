9c46628fe852fae97eebc315122d297b0041dc3a
Zach Miller <zchatk25966@gmail.com>
2022-05-14 14:10:59 -0700

    Started Challenge 3, got openCV working with the tutorial section, have to figure out a way to do heartbeat with a bigger camera

---
+++ Python/Lab 6/challenge3_webcam_ppg_tutorial.py
@@ -0,0 +1,54 @@
+# import cv2
+# import numpy as np
+# cap = cv2.VideoCapture(0,cv2.CAP_DSHOW) #==> use this instead if you’re on Windows!
+#
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
+# size = (width, height)
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('capture.avi', fourcc, 20.0, size)
+#
+# while(True):
+#   _, frame = cap.read()
+#   edges=cv2.Canny(frame,100,200)
+#   out.write(frame)
+#   cv2.imshow('Input', edges)
+#   c = cv2.waitKey(1)
+#   if c == 27:
+#     break
+# cap.release()
+# out.release()
+# cv2.destroyAllWindows()
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+
+ppg = []
+
+#cap = cv2.VideoCapture(0) # choose your appropriate camera!
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)# ==> use this instead if you’re on Windows!
+while(True):
+  _, frame = cap.read()
+  new_sample = frame.mean(axis=0).mean(axis=0)
+  new_sample = new_sample[2]  # replace the ? with index of the RED channel  openCV is in BGR format so b=0 g=1 r=2
+  ppg.append(-1* new_sample)  # append new_sample to ppg
+  cv2.imshow('Input', frame)
+  c = cv2.waitKey(1)
+  if c == 27:
+    break
+cap.release()
+cv2.destroyAllWindows()
+
+fig = plt.figure()
+ax = plt.axes()
+p = ppg[10:] # remove the first few points because of auto-exposure
+
+x = np.linspace(0, 1, len(p))
+ax.plot(x, p)
+plt.show()
+
+
+
+
