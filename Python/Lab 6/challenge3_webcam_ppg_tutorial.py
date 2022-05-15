c2688a26dd5bedb486fa02e95c6daa789c524f3f
Zach Miller <zchatk25966@gmail.com>
2022-05-15 11:17:04 -0700

    Challenge 3 almost done/done I cant tell, need to send to parter who has better camera

--- Python/Lab 6/challenge3_webcam_ppg_tutorial.py
+++ Python/Lab 6/challenge3_webcam_ppg_tutorial.py
@@ -23,9 +23,11 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 plt.style.use('seaborn-whitegrid')
 
 ppg = []
+timestamps= []
 
 #cap = cv2.VideoCapture(0) # choose your appropriate camera!
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)# ==> use this instead if you’re on Windows!
@@ -34,6 +36,7 @@ while(True):
   new_sample = frame.mean(axis=0).mean(axis=0)
   new_sample = new_sample[2]  # replace the ? with index of the RED channel  openCV is in BGR format so b=0 g=1 r=2
   ppg.append(-1* new_sample)  # append new_sample to ppg
+  timestamps.append(time.time_ns())
   cv2.imshow('Input', frame)
   c = cv2.waitKey(1)
   if c == 27:
@@ -43,11 +46,22 @@ cv2.destroyAllWindows()
 
 fig = plt.figure()
 ax = plt.axes()
-p = ppg[10:] # remove the first few points because of auto-exposure
+# p = ppg[10:] # remove the first few points because of auto-exposure
+#
+# x = np.linspace(0, 1, len(p))
+# ax.plot(x, p)
+# plt.show()
+p = ppg[10:]
+t = np.array(timestamps[10:]) # so we can easily subtract the first time
+t = t - t[0]
+t= t/(10**9)
+ax.set_xlabel("time(s)")
+ax.set_ylabel("Red Channel Value")
 
-x = np.linspace(0, 1, len(p))
-ax.plot(x, p)
+ax.plot(t, p)
 plt.show()
+
+
 
 
 
