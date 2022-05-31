a1b3c375052a4675598b4707b95a8eb3e1691f59
Zach Miller <zchatk25966@gmail.com>
2022-05-31 09:37:20 -0700

    updated arduino files

--- Project/ece16-space-invaders/controller/Arduino/SpaceInvadersController/Accelerometer.ino
+++ Project/ece16-space-invaders/controller/Arduino/SpaceInvadersController/Accelerometer.ino
@@ -13,10 +13,15 @@ const int Z_PIN = A2;
  * Set the "zero" states when each axis is neutral
  * NOTE: Customize this for your accelerometer sensor!
  */
-const int X_ZERO = 1850;
-const int Y_ZERO = 1850;
-const int Z_ZERO = 1950;
+const int X_ZERO = 2500;
+const int Y_ZERO = 2000;
+const int Z_ZERO = 2000;
 
+/*
+ * Set the "threshold" for the required angle for movement
+ */
+
+const int THRESHOLD = 15;
 
 /*
  * Configure the analog pins to be treated as inputs by the MCU
@@ -31,7 +36,7 @@ void setupAccelSensor() {
  * Read a sample from the accelerometer's 3 axes
  */
 void readAccelSensor() {
-  ax = analogRead(X_PIN); 
+  ax = analogRead(X_PIN);
   ay = analogRead(Y_PIN);
   az = analogRead(Z_PIN);
 }
@@ -54,21 +59,21 @@ int getOrientation() {
   int z = az - Z_ZERO;
 
   // If ax has biggest magnitude, it's either left or right
-  if(abs(x) >= abs(y) && abs(x) >= abs(z)) {
-    if( x < 0 ) // left
-      orientation = 3;
-    else
+  if(abs(z) >= abs(y) && abs(z) >= abs(x) && abs(z)>THRESHOLD) {
+    if( z < 0 ) // left
       orientation = 4;
+    else
+      orientation = 3;
   }
   // If ay has biggest magnitude, it's either up or down
-  else if(abs(y) >= abs(x) && abs(y) >= abs(z)) {
+  else if(abs(y) >= abs(x) && abs(y) >= abs(z) && abs(y)>THRESHOLD) {
     if( y < 0 ) // up
-      orientation = 1;
-    else // down
       orientation = 2;
+    else // down
+      orientation = 1;
   }
   // If az biggest magnitude, it's flat (or upside-down)
-  else if(abs(z) > abs(x) && abs(z) >= abs(y)) {
+  else if(abs(x) > abs(z) && abs(x) >= abs(y)) {
     orientation = 0; // flat
   }
 
--- Project/ece16-space-invaders/controller/Arduino/SpaceInvadersController/SpaceInvadersController.ino
+++ Project/ece16-space-invaders/controller/Arduino/SpaceInvadersController/SpaceInvadersController.ino
@@ -7,6 +7,15 @@ int ppg = 0;        // PPG from readPhotoSensor() (in Photodetector tab)
 int sampleTime = 0; // Time of last sample (in Sampling tab)
 bool sending;
 
+const int BUTTON_PIN=13;  //Pin that button is on
+bool conA=false;  //condition for button press
+int state=0;    //state of move speed
+
+int motorTime=0; //Time of start of Motor Buzz for on hit
+int currTime=0; //Time of currently
+
+const int buzzTime = 250; //ms to s
+
 /*
  * Initialize the various components of the wearable
  */
@@ -15,11 +24,14 @@ void setup() {
   setupCommunication();
   setupDisplay();
   setupPhotoSensor();
+  setupMotor();
+  pinMode(LED_BUILTIN, OUTPUT);
   sending = false;
 
   writeDisplay("Ready...", 1, true);
   writeDisplay("Set...", 2, false);
   writeDisplay("Play!", 3, false);
+  //Serial.begin(115200);
 }
 
 /*
@@ -27,6 +39,16 @@ void setup() {
  */
 void loop() {
   // Parse command coming from Python (either "stop" or "start")
+  //readAccelSensor();
+  //int b= getOrientation();
+  //Serial.print("orientation: ");
+  //Serial.println(String(b));
+  //Serial.print("ax: "); Serial.print(ax);
+  //Serial.print(" ay: "); Serial.print(ay);
+  //Serial.print(" az: "); Serial.println(az);
+  //readPhotoSensor();
+  //Serial.println(String(getTaps()));
+
   String command = receiveMessage();
   if(command == "stop") {
     sending = false;
@@ -36,9 +58,42 @@ void loop() {
     sending = true;
     writeDisplay("Controller: On", 0, true);
   }
+  else if(command == "dead"){
+    sending=true;
+    motorTime=millis();
+    //digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else{
+    sending=true;
+    writeDisplayCSV(command.c_str(),1);
+  }
+  currTime=millis();
 
   // Send the orientation of the board
   if(sending && sampleSensors()) {
-    sendMessage(String(getOrientation()));
+    String response = String(getOrientation()) + ",";
+    response += String(state) + ",";
+    response += String(getTaps());
+    // Type of Move , Speed of move, Shooting or not Shooting
+    sendMessage(response);
+
+    //sendMessage(String(getOrientation()));
+  }
+
+  if (digitalRead(BUTTON_PIN)==LOW){//Button Down
+    conA=true;
+  }
+  if (conA==true && digitalRead(BUTTON_PIN)==HIGH){//Button Released
+    conA=false;
+    state=state+1;
+    state=state%3;
+    //Switch between 3 states which are the spped of movement mode
+  }
+
+  if (currTime-motorTime <=buzzTime){
+    activateMotor(200);
+  }
+  else{
+    deactivateMotor();
   }
 }
